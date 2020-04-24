10670461dc859a22916589703f568fb56e65c0ef
uentity <unientity@gmail.com>
2020-04-25 01:06:25 +0500

    New style of options declaration

--- lib/vauth/ntlm.c
+++ lib/vauth/ntlm.c
@@ -644,7 +644,7 @@ CURLcode Curl_auth_create_ntlm_type3_message(struct Curl_easy *data,
     memcpy(tmp, &ntlm->nonce[0], 8);
     memcpy(tmp + 8, entropy, 8);
 
-    result = Curl_ssl_md5sum(tmp, 16, md5sum, MD5_DIGEST_LENGTH);
+    result = Curl_ssl_md5sum(tmp, 16, md5sum, CURL_MD5_DIGEST_LENGTH);
     if (!result) {
     /* We shall only use the first 8 bytes of md5sum, but the des code in
        Curl_ntlm_core_lm_resp only encrypt the first 8 bytes */
